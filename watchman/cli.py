c22c1d44edf7d643b89645587fe0d30c06fed0e0
Michael Ziegler <ziggi_0@web.de>
2015-11-29 21:57:27 +0100

    add module squad with class Watchman and RadioOperator. Add config option to cli

--- watchman/cli.py
+++ watchman/cli.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-This is a skeleton file that can serve as a starting point for a Python
-console script. To run this script uncomment the following line in the
-entry_points section in setup.cfg:
-
-    console_scripts =
-        hello_world = watchman.module:function
-
-Then run `python setup.py install` which will install the command `hello_world`
-inside your current environment.
-Besides console scripts, the header (i.e. until _logger...) of this file can
-also be used as template for Python modules.
-
-Note: This skeleton file can be safely removed if not needed!
-"""
 from __future__ import division, print_function, absolute_import
 
 import click
@@ -31,6 +16,7 @@ _logger = logging.getLogger(__name__)
 
 
 @click.command()
-def cli():
+@click.option('--config', '-c', type=str, help='path to config file')
+def cli(config):
     click.echo('Run watchman')
-
+    click.echo('Load config: {}'.format(config))
---
+++ watchman/squad.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+class Watchman(object):
+    """
+    A Watchman controls the output of a command
+    and will give an alert if something is wrong.
+    """
+    def __init__(self):
+        pass
+
+
+class RadioOperator(object):
+    """
+    A RadioOperator sends the alert given by a Watchman via email to the admin.
+    """
+    def __init__(self):
+        pass
