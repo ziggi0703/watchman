ac0529eb0008ed1047dec369435bf65f4f408b84
Michael Ziegler <ziggi_0@web.de>
2015-11-30 10:22:24 +0100

    add RadioOperator and function to send an mail to admin

--- watchman/cli.py
+++ watchman/cli.py
@@ -5,18 +5,55 @@ from __future__ import division, print_function, absolute_import
 import click
 import sys
 import logging
+from logging.handlers import SysLogHandler
+import daemon
+import time
 
 from watchman import __version__
+from watchman.squad import PingGuard, RadioOperator
 
 __author__ = "Michael Ziegler"
 __copyright__ = "Michael Ziegler"
 __license__ = "none"
 
-_logger = logging.getLogger(__name__)
+_logger = logging.getLogger()
+#_handler = SysLogHandler(address='/dev/log')
 
 
 @click.command()
-@click.option('--config', '-c', type=str, help='path to config file')
+@click.option('--config', '-c', type=str, default=None, help='path to config file')
 def cli(config):
-    click.echo('Run watchman')
-    click.echo('Load config: {}'.format(config))
+    with daemon.DaemonContext():
+        _handler = logging.FileHandler('/home/michael/logs/watchman.log')
+        _formatter = logging.Formatter(fmt='[%(asctime)s][%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        _handler.setFormatter(_formatter)
+        _logger.addHandler(_handler)
+        _logger.setLevel(logging.DEBUG)
+
+        _logger.info('Start watchman')
+
+        if config is None:
+            _logger.info('Load default config.')
+            with open('/home/michael/PycharmProjects/watchman/watchman/config/default.py', 'r') as config_file:
+                config = eval(config_file.read())
+        else:
+            _logger.info('Load config: {}'.format(config))
+            with open(config) as config_file:
+                config = eval(config_file.read())
+
+        _logger.info('Config: {}'.format(config))
+    #    with daemon.DaemonContext(files_preserve=[_handler.stream]):
+        _logger.debug('Start ping guards')
+        ping_guards = [PingGuard('PingGuard {}'.format(index), host=hostname)
+                       for index, hostname in enumerate(config['hosts'])]
+        rto = RadioOperator('RTO1', admin_mail=config['admin_mail'])
+        while True:
+            alerts = []
+            _logger.debug('PingGuards checking again.')
+            for guard in ping_guards:
+                guard.guard(alerts)
+
+            if len(alerts) > 0:
+                rto.send_alerts(alerts)
+
+            time.sleep(5)
--- watchman/squad.py
+++ watchman/squad.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env
+import os
 import subprocess
 
+import datetime
 from abc import ABCMeta, abstractmethod, abstractproperty
 import logging
+import cStringIO
+from email.mime.text import MIMEText
+import smtplib
 _logger = logging.getLogger(__name__)
 
 
@@ -26,19 +31,24 @@ class Watchman(object):
     def __str__(self):
         return '<{}: {}>'.format(self.__class__, self._name)
 
-    def guard(self):
+    def guard(self, alerts):
         """
         Start the watch for the Watchman.
+
+        :param alerts: watchman adds his alerts to it
+        :type alerts: list
         """
-        _logger.info('{} starts the watch.'.format(self._name))
-        _logger.info('Check command: {}'.format(self._command))
+        _logger.debug('{} starts the watch.'.format(self._name))
+        _logger.debug('Check command: {}'.format(self._command))
         process = subprocess.Popen(self._command, stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
         out, error = process.communicate()
         rc = process.returncode
 
-        self._check_output(rc, out, error)
+        own_alerts = self._check_output(rc, out, error)
 
+        if len(own_alerts) > 0:
+            alerts.append(own_alerts)
 
     @abstractmethod
     def _check_output(self, return_code, out, error):
@@ -53,6 +63,9 @@ class Watchman(object):
 
         :param error: stderr of the command
         :type error: str
+
+        :return: list with alerts: [(guard_name, command, return_code, error message)]
+        :rtype: list
         """
         raise NotImplementedError
 
@@ -87,14 +100,46 @@ class PingGuard(Watchman):
 
     def _check_output(self, return_code, out, error):
         if return_code == 0:
-            return True
+            return []
         else:
-            return
+            return [self._name, self.command, return_code, error]
 
 
 class RadioOperator(object):
     """
     A RadioOperator sends the alert given by a Watchman via email to the admin.
     """
-    def __init__(self):
-        pass
+    def __init__(self, name, admin_mail):
+        self._name = name
+        self._admin_mail = admin_mail
+        self._host = os.getenv('HOST') if os.getenv('HOSTNAME') is None else os.getenv('HOSTNAME')
+
+    def send_alerts(self, alerts):
+        """
+        Send an alerts to the admin via email.
+
+        :param alerts: list of alerts
+        :type alerts: list
+        """
+        mail = cStringIO.StringIO()
+        mail.write('Dear Admin,\nat {} some errors occured:\n\n'.format(datetime.
+                                                                        datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+
+        for alert in alerts:
+            alert_message = 'Guard {} with command {} observed return state {} and error message {}.\n'.format(*alert)
+            mail.write(alert_message)
+
+        mail.write('\n\nPlease take your actions...\n')
+
+        subject = 'Errors on host {}.'.format(self._host)
+
+        _logger.info('Send alerts to admin {}'.format(self._admin_mail))
+
+        message = MIMEText(mail.getvalue())
+        message['From'] = '{}@{}'.format(self._name, self._host)
+        message['To'] = self._admin_mail
+        message['Subject'] = subject
+
+        sender = smtplib.SMTP('localhost')
+        sender.sendmail(message['From'], message['To'], message.as_string())
+        sender.quit()
