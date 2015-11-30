74797217073bfad827b59af1074feb9394776b85
Michael Ziegler <ziggi_0@web.de>
2015-11-30 13:57:40 +0100

    add config option sleep. Add command option as_daemon to the cli. Add stream handler

--- watchman/cli.py
+++ watchman/cli.py
@@ -21,38 +21,49 @@ _logger = logging.getLogger()
 
 
 @click.command()
+@click.option('--as_daemon', '-d', is_flag=True, default=False, help='Start watchman as a daemon.')
 @click.option('--config', '-c', type=str, default=None, help='path to config file')
-def cli(config):
-    with daemon.DaemonContext():
-        _handler = logging.FileHandler('/home/michael/logs/watchman.log')
-        _formatter = logging.Formatter(fmt='[%(asctime)s][%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-        _handler.setFormatter(_formatter)
-        _logger.addHandler(_handler)
-        _logger.setLevel(logging.WARNING)
+def cli(config, as_daemon):
+    if as_daemon:
+        with daemon.DaemonContext():
+            run(config)
+    else:
+        run(config)
 
-        _logger.info('Start watchman')
 
-        if config is None:
-            _logger.info('Load default config.')
-            with open('/home/michael/PycharmProjects/watchman/watchman/config/default.py', 'r') as config_file:
-                config = eval(config_file.read())
-        else:
-            _logger.info('Load config: {}'.format(config))
-            with open(config) as config_file:
-                config = eval(config_file.read())
+def run(config):
+    _handler = logging.FileHandler('/home/michael/logs/watchman.log')
+    _stream_handler = logging.StreamHandler()
+    _formatter = logging.Formatter(fmt='[%(asctime)s][%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    _stream_handler.setFormatter(_formatter)
+    _handler.setFormatter(_formatter)
+    _logger.addHandler(_handler)
+    _logger.addHandler(_stream_handler)
+    _logger.setLevel(logging.DEBUG)
 
-        _logger.info('Config: {}'.format(config))
-        _logger.debug('Start ping guards')
-        ping_guards = [PingGuard('PingGuard {}'.format(index), host=hostname)
-                       for index, hostname in enumerate(config['hosts'])]
-        rto = RadioOperator('RTO1', admin_mail=config['admin_mail'])
-        while True:
-            alerts = []
-            _logger.debug('PingGuards checking again.')
-            for guard in ping_guards:
-                guard.guard(alerts)
+    _logger.info('Start watchman')
 
-            if len(alerts) > 0:
-                rto.send_alerts(alerts)
+    if config is None:
+        _logger.info('Load default config.')
+        with open('/home/michael/PycharmProjects/watchman/watchman/config/default.py', 'r') as config_file:
+            config = eval(config_file.read())
+    else:
+        _logger.info('Load config: {}'.format(config))
+        with open(config) as config_file:
+            config = eval(config_file.read())
 
-            time.sleep(600)
+    _logger.info('Config: {}'.format(config))
+    _logger.debug('Start ping guards')
+    ping_guards = [PingGuard('PingGuard {}'.format(index), host=hostname)
+                   for index, hostname in enumerate(config['hosts'])]
+    rto = RadioOperator('RTO1', admin_mail=config['admin_mail'])
+    while True:
+        alerts = []
+        _logger.debug('PingGuards checking again.')
+        for guard in ping_guards:
+            guard.guard(alerts)
+
+        if len(alerts) > 0:
+            rto.send_alerts(alerts)
+
+        time.sleep(config['sleep'])
--- watchman/config/default.py
+++ watchman/config/default.py
@@ -1,4 +1,10 @@
 {
+    # Hosts which are checked
     'hosts': ['ekpbelle', 'ekpblus001', 'ekpblus002', 'ekpblus003', 'ekpblus007'],
-    'admin_mail': 'michael.ziegler2@kit.edu'
+
+    # Send the error mail to the admin
+    'admin_mail': 'michael.ziegler2@kit.edu',
+
+    # duration between two checks in seconds
+    'sleep': 10
 }
